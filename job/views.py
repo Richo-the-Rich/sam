f0e817ace90005066c95c7536b3dc9522ef05dbf
ich07 <ulrichdakaz@gmail.com>
2023-02-23 00:12:49 +0530

    Fixing bugs

--- job/views.py
+++ job/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_job(request):
-    context = {}
-    return render(request, 'job/index.html', context)
+    return render(request, 'index.html')
